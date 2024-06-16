460105b5fd921b3aeaee8dc1c1ef207b28eb6ba2
Felipeoliveeira <Felipeoliveira.aju79@gmail.com>
2024-06-16 12:07:27 -0300

    Primeiros passos React

---
+++ first-project-react/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+// Componente em classe é uma classe que herda o component do React,
+// e retorna HTML do método render
+class App extends React.Component{
+  // Método responsável por renderizar o conteúdo HTML do nosso componente 
+  render(){
+    return (
+      <div>
+        <h1>Aprendendo React</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
